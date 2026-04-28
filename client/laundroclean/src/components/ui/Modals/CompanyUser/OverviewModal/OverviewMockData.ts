57c4eeb0c6155c9d9cf4cf939e5a011dd2b00dce
merytpeters <merytpeters@gmail.com>
2026-04-28 13:00:16 +0100

    Separated overview mock data

---
+++ client/laundroclean/src/components/ui/Modals/CompanyUser/OverviewModal/OverviewMockData.ts
@@ -0,0 +1,65 @@
+
+export const stats = {};
+export const bookingScheduleData = [
+    {
+        id: "1",
+        time: "09:00 AM",
+        customerName: "John Doe",
+        serviceType: "Laundry Wash",
+        status: "COMPLETED",
+        deliveryType: "DROP_OFF"
+    },
+    {
+        id: "2",
+        time: "09:00 AM",
+        customerName: "Michael Doe",
+        serviceType: "Laundry Wash",
+        status: "CUSTOMER_PICKED_UP_FROM_POINT",
+        deliveryType: "PICK_UP"
+    },
+    {
+        id: "3",
+        time: "10:00 AM",
+        customerName: "John Doe",
+        serviceType: "Laundry Wash",
+        status: "COMPANY_DROPPED_OFF_AT_POINT",
+        deliveryType: "DROP_OFF"
+    },
+    {
+        id: "4",
+        time: "09:00 AM",
+        customerName: "John Doe",
+        serviceType: "Laundry Wash",
+        status: "DELIVERED",
+        deliveryType: "PICK_UP"
+    },
+]    
+
+    
+export const clientInfoData = [
+    {
+        id: "1",      
+        customerName: "Michael Doe",
+        email: "michael@test.com",
+        clientStatus: "active"
+    },
+    {
+        id: "2",
+        customerName: "Maria Doe",
+        email: "maria@test.com",
+        clientStatus: "inactive"
+    },
+    {
+        id: "3",
+        customerName: "John Doe",
+        email: "john@test.com",
+        clientStatus: "active"
+    },
+    {
+        id: "4",
+        customerName: "Merlin Doe",
+        email: "merlin@test.com",
+        clientStatus: "active"
+    }
+]
+    
