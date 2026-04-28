285f12dd48515f6db4bdc83fbc122ead0a61fc43
merytpeters <merytpeters@gmail.com>
2026-04-28 10:54:00 +0100

    Overview modal with Schedule UI

--- client/laundroclean/src/components/ui/Modals/CompanyUser/OverviewModal/OverviewModal.tsx
+++ client/laundroclean/src/components/ui/Modals/CompanyUser/OverviewModal/OverviewModal.tsx
@@ -4,16 +4,66 @@ import { roleConfig } from "src/lib/company-user/role-config";
 import { useCompanyUserMenu } from "src/components/layouts/CompanyUser/context/CompanyUserMenuContext";
 import { statMeta } from "src/components/ui/StatMeta";
 import styles from "./OverviewModal.module.css"
+import ScheduleCard from "src/components/ui/flexboxes/ScheduleCard";
+import { mapDeliveryType, mapBookingStatus } from "src/types/bookingStatus";
 
 export default function Overview() {
     const { user } = useCompanyUserMenu();
 
     const config = roleConfig[user.role];
     const stats = {}; // Replace with actual stats data source
+    const bookingScheduleData = [
+        {
+            id: "1",
+            time: "09:00 AM",
+            customerName: "John Doe",
+            serviceType: "Laundry Wash",
+            status: "COMPLETED",
+            deliveryType: "DROP_OFF"
+        },
+        {
+            id: "2",
+            time: "09:00 AM",
+            customerName: "Michael Doe",
+            serviceType: "Laundry Wash",
+            status: "CUSTOMER_PICKED_UP_FROM_POINT",
+            deliveryType: "PICK_UP"
+        },
+        {
+            id: "3",
+            time: "10:00 AM",
+            customerName: "John Doe",
+            serviceType: "Laundry Wash",
+            status: "COMPANY_DROPPED_OFF_AT_POINT",
+            deliveryType: "DROP_OFF"
+        },
+        {
+            id: "4",
+            time: "09:00 AM",
+            customerName: "John Doe",
+            serviceType: "Laundry Wash",
+            status: "DELIVERED",
+            deliveryType: "PICK_UP"
+        },
+    ]
+
+    const mappedDelivery =bookingScheduleData.map((item) => ({
+        ...item,
+        deliveryType: mapDeliveryType(item.deliveryType)
+    }))
+
+    console.log(mappedDelivery);
+
+    const mappedSchedule = mappedDelivery.map((item) => ({
+        ...item,
+        status: mapBookingStatus(item.status, { deliveryType: item?.deliveryType}),
+    }))
+
+    console.log(mappedSchedule);
     
     return (
         <div className={styles.overviewmodalcontainer}>
-            <section className={styles.statcardbox}>
+            <section aria-label="Analysis Card" className={styles.statcardbox}>
                 {config.statCards?.map((card) => {
                     const meta = statMeta[card.key as keyof typeof statMeta];
 
@@ -28,6 +78,11 @@ export default function Overview() {
                         />
                     );
                 })}
+            </section>
+               
+            <section aria-label="Information Section" className={styles.infosection}>
+                <ScheduleCard items={mappedSchedule}/>
+                Customer Info
             </section>
         </div>
     );
