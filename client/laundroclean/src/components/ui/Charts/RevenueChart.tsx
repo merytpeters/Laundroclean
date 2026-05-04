ee372d332619229229f442161a8b5d606363b40e
merytpeters <merytpeters@gmail.com>
2026-05-04 18:31:44 +0100

    Feat(UI): control panel overview page scaffolding

---
+++ client/laundroclean/src/components/ui/Charts/RevenueChart.tsx
@@ -0,0 +1,15 @@
+/*"use client";
+
+import {
+    LineChart
+}
+
+from "recharts";*/
+
+export default function RevenueChart () {
+    return (
+        <div style={{ color: "#000"}}>
+            Gross Revenue, Net Revenue, Profit, Expenses
+        </div>
+    )
+}
