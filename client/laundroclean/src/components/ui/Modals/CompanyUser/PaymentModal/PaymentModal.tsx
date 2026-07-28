5b8fd5d82ec24c31d8f31404bc6c84accf81b362
merytpeters <merytpeters@gmail.com>
2026-07-28 19:00:56 +0100

    Fix: styling and ui fixes

--- client/laundroclean/src/components/ui/Modals/CompanyUser/PaymentModal/PaymentModal.tsx
+++ client/laundroclean/src/components/ui/Modals/CompanyUser/PaymentModal/PaymentModal.tsx
@@ -6,105 +6,101 @@ import DebitCardUI from "src/components/ui/PaymentUI/DebitCardUI";
 import { CompanyUser, Client } from "src/types/users/user";
 
 type PaymentTab = {
-  key: "card" | "transfer" | "pos" | "wallet" | "cash";
-  label: string;
-  img: string;
-  component: React.ReactNode;
+    key: "card" | "transfer" | "pos" | "wallet" | "cash";
+    label: string;
+    img: string;
+    component: React.ReactNode;
 };
 
 const paymentTabs: PaymentTab[] = [
-  {
-    key: "card",
-    label: "Credit / Debit Card",
-    img: "/img/creditcardicon.png",
-    component: <DebitCardUI />,
-  },
-  {
-    key: "cash",
-    label: "Cash",
-    img: "",
-    component: "",
-  },
-  {
-    key: "transfer",
-    label: "Bank Transfer",
-    img: "/img/bankbuildingicon.png",
-    component: "",
-  },
-  {
-    key: "wallet",
-    label: "OPay Wallet",
-    img: "/img/opayicon.png",
-    component: "",
-  },
-  {
-    key: "pos",
-    label: "POS Machine",
-    img: "/img/posicon.png",
-    component: "",
-  },
+    {
+        key: "card",
+        label: "Credit / Debit Card",
+        img: "/img/creditcardicon.png",
+        component: <DebitCardUI />,
+    },
+    {
+        key: "cash",
+        label: "Cash",
+        img: "/img/cash-on-delivery.png",
+        component: "",
+    },
+    {
+        key: "transfer",
+        label: "Bank Transfer",
+        img: "/img/bankbuildingicon.png",
+        component: "",
+    },
+    {
+        key: "wallet",
+        label: "OPay Wallet",
+        img: "/img/payment.png",
+        component: "",
+    },
+    {
+        key: "pos",
+        label: "POS Machine",
+        img: "/img/pos-terminal.png",
+        component: "",
+    },
 ];
 
 type PaymentProps = {
-  usertype: CompanyUser | Client;
+    usertype: CompanyUser | Client;
 };
 
 export default function PaymentModal({ usertype }: PaymentProps) {
-  const isCompany = usertype?.type === "COMPANYUSER";
-  console.log(usertype);
+    const isCompany = usertype?.type === "COMPANYUSER";
+    console.log(usertype);
 
-  const tabs = paymentTabs.filter((tab) => {
-  if (isCompany) {
-    return ["pos", "cash", "transfer", "wallet"].includes(tab.key);
-  }
+    const tabs = paymentTabs.filter((tab) => {
+        return isCompany
+            ? ["pos", "cash", "transfer", "wallet"].includes(tab.key)
+            : ["card", "transfer", "wallet"].includes(tab.key);
+    });
 
-  return tab.key !== "pos";
-});
+    const [activeTab, setActiveTab] = useState<PaymentTab["key"]>(tabs[0].key);
 
-  const [activeTab, setActiveTab] = useState<PaymentTab["key"]>(tabs[0].key);
+    return (
+        <div className={styles.paymentmodalcontainer}>
+            <strong className={styles.headertext}>Payment Methods</strong>
 
-  return (
-    <div className={styles.paymentmodalcontainer}>
-      <strong className={styles.headertext}>Payment Methods</strong>
+            <section className={styles.headertab}>
+                <select
+                    className={styles.tabSelect}
+                    value={activeTab}
+                    onChange={(e) => setActiveTab(e.target.value as PaymentTab["key"])}
+                >
+                    {tabs.map((tab) => (
+                        <option key={tab.key} value={tab.key}>
+                            {tab.label}
+                        </option>
+                    ))}
+                </select>
 
-      <section className={styles.headertab}>
-        <select
-          className={styles.tabSelect}
-          value={activeTab}
-          onChange={(e) => setActiveTab(e.target.value as PaymentTab["key"])}
-        >
-          {tabs.map((tab) => (
-            <option key={tab.key} value={tab.key}>
-              {tab.label}
-            </option>
-          ))}
-        </select>
+                {tabs.map((tab) => (
+                    <button
+                        key={tab.key}
+                        onClick={() => setActiveTab(tab.key)}
+                        className={`${styles.tabbtn} ${activeTab === tab.key ? styles.active : ""
+                            }`}
+                    >
+                        <span className={styles.iconnlabel}>
+                            <img src={tab.img} width={25} height={25} alt="" />
+                            {tab.label}
+                        </span>
 
-        {tabs.map((tab) => (
-          <button
-            key={tab.key}
-            onClick={() => setActiveTab(tab.key)}
-            className={`${styles.tabbtn} ${
-              activeTab === tab.key ? styles.active : ""
-            }`}
-          >
-            <span className={styles.iconnlabel}>
-              <img src={tab.img} width={20} height={20} alt="" />
-              {tab.label}
-            </span>
+                        <hr
+                            className={`${styles.simpleline} ${activeTab === tab.key ? styles.simplelineactive : ""
+                                }`}
+                        />
+                    </button>
+                ))}
+            </section>
 
-            <hr
-              className={`${styles.simpleline} ${
-                activeTab === tab.key ? styles.simplelineactive : ""
-              }`}
-            />
-          </button>
-        ))}
-      </section>
-
-      <section className={styles.activetabcontainer}>
-        {tabs.find((tab) => tab.key === activeTab)?.component}
-      </section>
-    </div>
-  );
+            <section className={styles.activetabcontainer}>
+                {tabs.find((tab) => tab.key === activeTab)?.component}
+            </section>
+        </div>
+    );
 }
